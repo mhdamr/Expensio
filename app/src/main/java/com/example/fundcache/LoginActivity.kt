49265265e7eeae367ca69f200a5fd4fdd2e28bb3
mhdamr <B1802034@live.tees.ac.uk>
2023-02-27 01:28:32 +0000

    Added Firebase authentication system successfully. User data (email & password) is now stored in Firebase.

--- app/src/main/java/com/example/fundcache/LoginActivity.kt
+++ app/src/main/java/com/example/fundcache/LoginActivity.kt
@@ -1,11 +1,89 @@
 package com.example.fundcache
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        // Get references to UI elements
+        emailEditText = findViewById(R.id.editTextEmail)
+        passwordEditText = findViewById(R.id.editTextPassword)
+        loginButton = findViewById(R.id.buttonLogin)
+        registerButton = findViewById(R.id.buttonRegister)
+        progressBar = findViewById(R.id.progressBar)
+
+        // Set click listener for login button
+        loginButton.setOnClickListener {
+            loginUser()
+        }
+
+        // Set click listener for register button
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun loginUser() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        if (email.isEmpty()) {
+            emailEditText.error = "Email is required"
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.error = "Please enter a valid email"
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            passwordEditText.error = "Password is required"
+            passwordEditText.requestFocus()
+            return
+        }
+
+        progressBar.visibility = View.VISIBLE
+
+        // Authenticate the user with Firebase Auth
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Login successful, navigate to MainActivity
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Login failed, display error message
+                    Toast.makeText(baseContext, "Login failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+
+                progressBar.visibility = View.GONE
+            }
     }
 }
--- app/src/main/java/com/example/fundcache/RegisterActivity.kt
+++ app/src/main/java/com/example/fundcache/RegisterActivity.kt
@@ -2,10 +2,87 @@ package com.example.fundcache
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+import android.util.Patterns
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var registerButton: Button
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        // Get references to UI elements
+        registerButton = findViewById(R.id.buttonRegister)
+        emailEditText = findViewById(R.id.editTextEmail)
+        passwordEditText = findViewById(R.id.editTextPassword)
+        progressBar = findViewById(R.id.progressBar)
+
+        // Set click listener for register button
+        registerButton.setOnClickListener {
+            registerUser()
+        }
     }
+
+    private fun registerUser() {
+        val email = emailEditText.text.toString().trim()
+        val password = passwordEditText.text.toString().trim()
+
+        if (email.isEmpty()) {
+            emailEditText.error = "Email is required"
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.error = "Please enter a valid email"
+            emailEditText.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            passwordEditText.error = "Password is required"
+            passwordEditText.requestFocus()
+            return
+        }
+
+        if (password.length < 6) {
+            passwordEditText.error = "Password must be at least 6 characters long"
+            passwordEditText.requestFocus()
+            return
+        }
+
+        progressBar.visibility = View.VISIBLE
+
+        // Register the user with Firebase Auth
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Registration successful, navigate to MainActivity
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // Registration failed, display error message
+                    Toast.makeText(baseContext, "Registration failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+
+                progressBar.visibility = View.GONE
+            }
+    }
+
 }
