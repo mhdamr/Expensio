d73b02403a1398d37fd952b44b25f0f6c1f74fce
Thilaanraj <1191202463@student.mmu.edu.my>
2023-04-12 18:37:56 +0100

    Added ExpenseFragment.kt, added fragment_expense.xml layout . Modified nav_graph.xml and WalletDetailFragment.kt to add expense fragment

---
+++ app/src/main/java/com/example/fundcache/ExpenseFragment.kt
@@ -0,0 +1,140 @@
+package com.example.fundcache
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ExpenseFragment : Fragment() {
+    private lateinit var amountEditText: EditText
+    private lateinit var descriptionEditText: EditText
+    private lateinit var saveButton: Button
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var walletId: String
+
+    private lateinit var recurrenceSpinner: Spinner
+    private lateinit var recurrenceOption: String
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_expense, container, false)
+
+        // Get a reference to the Firebase Firestore and FirebaseAuth objects
+        db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        currentUser = auth.currentUser!!
+
+        // Get the walletId argument passed from WalletDetailFragment
+        walletId = arguments?.getString("walletId") ?: ""
+
+        // Get references to the UI components
+        amountEditText = view.findViewById(R.id.amount_edittext)
+        descriptionEditText = view.findViewById(R.id.description_edittext)
+        saveButton = view.findViewById(R.id.save_button)
+
+        // Set a click listener for the save button
+        saveButton.setOnClickListener {
+            saveExpense()
+        }
+
+        recurrenceSpinner = view.findViewById(R.id.recurrence_spinner)
+        recurrenceSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                recurrenceOption = parent?.getItemAtPosition(position).toString()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                recurrenceOption = "Never"
+            }
+        }
+
+
+        Log.d("MyApp", "Wallet ID: $walletId")
+        return view
+    }
+
+    private fun saveExpense() {
+        val amount = amountEditText.text.toString().toDoubleOrNull()
+        val description = descriptionEditText.text.toString()
+
+        // Validate the input
+        if (amount == null || description.isEmpty()) {
+            Toast.makeText(context, "Please enter valid expense data.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Create a new expense object
+        val expense = hashMapOf(
+            "amount" to amount,
+            "description" to description,
+            "type" to "expense",
+            "timestamp" to FieldValue.serverTimestamp(),
+        )
+
+
+        // Add the expense to the selected wallet's transactions collection
+        db.collection("users").document(currentUser.uid)
+            .collection("wallets")
+            .document(walletId)
+            .collection("transactions")
+            .add(expense)
+            .addOnSuccessListener {
+                Toast.makeText(context, "Expense added successfully.", Toast.LENGTH_SHORT).show()
+                if (recurrenceOption != "Never") {
+                    saveRecurrence()
+                }
+                activity?.onBackPressed()
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "Error adding expense.", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun saveRecurrence() {
+        val amount = amountEditText.text.toString().toDoubleOrNull()
+        val description = descriptionEditText.text.toString()
+
+        // Validate the input
+        if (amount == null || description.isEmpty()) {
+            Toast.makeText(context, "Please enter valid expense data.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // Create a new expense object
+        val expense = hashMapOf(
+            "amount" to amount,
+            "description" to description,
+            "type" to "expense",
+            "timestamp" to FieldValue.serverTimestamp(),
+            "recurrence" to recurrenceOption
+        )
+
+
+        // Add the expense to the selected wallet's transactions collection
+        db.collection("users").document(currentUser.uid)
+            .collection("wallets")
+            .document(walletId)
+            .collection("recurrence")
+            .add(expense)
+/*            .addOnSuccessListener {
+                Toast.makeText(context, "expense added successfully.", Toast.LENGTH_SHORT).show()
+                activity?.onBackPressed()
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, "Error adding expense.", Toast.LENGTH_SHORT).show()
+            }*/
+    }
+
+}
