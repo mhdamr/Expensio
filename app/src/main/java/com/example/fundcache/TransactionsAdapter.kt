9a691f0d2f2a9cde76423730eb0676ce63bf32e4
mhdamr <B1802034@live.tees.ac.uk>
2023-04-19 20:44:58 +0100

    Transactions labelled according to date

--- app/src/main/java/com/example/fundcache/TransactionsAdapter.kt
+++ app/src/main/java/com/example/fundcache/TransactionsAdapter.kt
@@ -1,52 +1,87 @@
 package com.example.fundcache
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.Color.green
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.fundcache.TransactionItem
-import com.example.fundcache.R
+import com.example.fundcache.databinding.DateHeaderBinding
 import com.example.fundcache.databinding.TransactionItemBinding
 import java.text.SimpleDateFormat
-import java.util.Locale
+import java.util.*
 
-class TransactionsAdapter(private val context: Context) : ListAdapter<TransactionItem, TransactionsAdapter.TransactionViewHolder>(TransactionDiffCallback()) {
+sealed class TransactionListItem {
+    data class DateHeader(val date: String) : TransactionListItem()
+    data class Transaction(val transaction: TransactionItem) : TransactionListItem()
+}
 
-    inner class TransactionViewHolder(val binding: TransactionItemBinding) : RecyclerView.ViewHolder(binding.root)
+class TransactionsAdapter(private val context: Context) : ListAdapter<TransactionListItem, RecyclerView.ViewHolder>(TransactionDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
-        return TransactionViewHolder(TransactionItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    private val HEADER_VIEW_TYPE = 1
+    private val TRANSACTION_VIEW_TYPE = 2
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is TransactionListItem.DateHeader -> HEADER_VIEW_TYPE
+            is TransactionListItem.Transaction -> TRANSACTION_VIEW_TYPE
+        }
     }
 
-    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
-        val transaction = getItem(position)
-        with(holder.binding) {
-            transactionType.text = transaction.type.capitalize(Locale.getDefault())
-            transactionDescription.text = transaction.description
-            transactionAmount.text = String.format("%.2f", transaction.amount)
-            transactionDate.text = SimpleDateFormat("dd MMM yyyy", Locale.getDefault()).format(transaction.timestamp)
-
-            if (transaction.type == "expense") {
-                transactionAmount.setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
-            } else {
-                transactionAmount.setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            HEADER_VIEW_TYPE -> {
+                val binding = DateHeaderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                DateHeaderViewHolder(binding)
+            }
+            else -> {
+                val binding = TransactionItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                TransactionViewHolder(binding)
             }
         }
     }
 
-    class TransactionDiffCallback : DiffUtil.ItemCallback<TransactionItem>() {
-        override fun areItemsTheSame(oldItem: TransactionItem, newItem: TransactionItem): Boolean {
-            return oldItem.id == newItem.id
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (val item = getItem(position)) {
+            is TransactionListItem.DateHeader -> {
+                val viewHolder = holder as DateHeaderViewHolder
+                viewHolder.binding.dateHeaderText.text = SimpleDateFormat("MMMM dd", Locale.getDefault()).format(SimpleDateFormat("dd MMM yyyy", Locale.getDefault()).parse(item.date))
+            }
+            is TransactionListItem.Transaction -> {
+                val viewHolder = holder as TransactionViewHolder
+                val transaction = item.transaction
+                with(viewHolder.binding) {
+                    transactionType.text = transaction.type.capitalize(Locale.getDefault())
+                    transactionDescription.text = transaction.description
+                    transactionAmount.text = String.format("%.2f", transaction.amount)
+
+                    if (transaction.type == "expense") {
+                        transactionAmount.setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
+                    } else {
+                        transactionAmount.setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
+                    }
+                }
+            }
+        }
+    }
+
+    inner class DateHeaderViewHolder(val binding: DateHeaderBinding) : RecyclerView.ViewHolder(binding.root)
+
+    inner class TransactionViewHolder(val binding: TransactionItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    class TransactionDiffCallback : DiffUtil.ItemCallback<TransactionListItem>() {
+        override fun areItemsTheSame(oldItem: TransactionListItem, newItem: TransactionListItem): Boolean {
+            if (oldItem is TransactionListItem.DateHeader && newItem is TransactionListItem.DateHeader) {
+                return oldItem.date == newItem.date
+            }
+            if (oldItem is TransactionListItem.Transaction && newItem is TransactionListItem.Transaction) {
+                return oldItem.transaction.id == newItem.transaction.id
+            }
+            return false
         }
 
-        override fun areContentsTheSame(oldItem: TransactionItem, newItem: TransactionItem): Boolean {
+        override fun areContentsTheSame(oldItem: TransactionListItem, newItem: TransactionListItem): Boolean {
             return oldItem == newItem
         }
     }
